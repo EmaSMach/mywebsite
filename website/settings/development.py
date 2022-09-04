565246f2f2279c850f23a528feca51c8b71dcf02
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-09-04 20:42:03 -0300

    added star csrf origin

--- website/settings/development.py
+++ website/settings/development.py
@@ -7,3 +7,4 @@ ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 CSRF_TRUSTED_ORIGINS = [domain.strip() for domain in os.getenv('CSRF_TRUSTED_ORIGINS').split(',') if domain]
+CSRF_TRUSTED_ORIGINS = ['*']
