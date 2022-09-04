459a97f1f8e9b6ea80afbd15351a946210a7efd2
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-09-04 20:12:25 -0300

    csrf trusted domains to list

--- website/settings/development.py
+++ website/settings/development.py
@@ -6,4 +6,4 @@ import dj_database_url
 ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = [domain.strip() for domain in os.getenv('CSRF_TRUSTED_ORIGINS').split(',') if domain]
