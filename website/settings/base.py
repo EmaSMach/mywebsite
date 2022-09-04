483e7b2158ac8ce7bdc8f6da539a03d00134765e
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-09-04 19:22:03 -0300

    added static files folders config

--- website/settings/base.py
+++ website/settings/base.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR.parent / 'static',
+]
+
+STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
