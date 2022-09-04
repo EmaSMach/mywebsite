72982de73868832f47d96fe8d55072975668389b
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-09-04 19:24:34 -0300

    added config for media files

--- website/settings/base.py
+++ website/settings/base.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.parent / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
