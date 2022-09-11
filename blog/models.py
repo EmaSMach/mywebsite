84726651e5414ea6dc6536544b460bc00cfb05d8
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-09-11 00:39:11 -0300

    remove unused imports

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,6 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 
 
